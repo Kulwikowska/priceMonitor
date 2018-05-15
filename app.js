6f5f9fb2b06b8bbbca05de369c8ef10c77a8ed9f
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-05-15 11:22:12 +0200

    passport authentication added

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use('/libs', express.static(__dirname + "/node_modules"));
 app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
 //requiring routes 
 indexRoutes = require("./routes/index");
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,55 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
+var User = require("../models/user");
 
 //root route 
 router.get("/", function(req, res) {
     res.render("index");
 });
 
+//show register form
+router.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//home 
+router.get("/dashboard", function(req, res) {
+    res.render("home/dashboard");
+});
+
+//handle sign up logic
+router.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register", { eroor: err.message });
+        }
+        passport.authenticate("local")(req, res, function() {
+            req.flash("success", "Welcome to Crowler! " + req.body.username);
+            res.redirect("/dashboard");
+        });
+    });
+});
+
 //show login form 
 router.get("/login", function(req, res) {
     res.render("login");
+});
+
+//handling login logic
+router.post("/login", passport.authenticate("local", {
+    succesRedirect: "/dashboard",
+    failureRedirect: "/login"
+}), function(req, res) {});
+
+
+//logout route 
+router.get("/logout", function(req, res) {
+    req.logout();
+    req.flash("success", "Logged you out!");
+    res.redirect("/");
 });
 
 module.exports = router;
